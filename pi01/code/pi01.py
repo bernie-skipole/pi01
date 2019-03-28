fc1dbf374122d52923b1c6295a33cb8a692f99d4
skipole <bernie@skipole.co.uk>
2019-03-28 16:49:32 +0000

    created pi01.py

---
+++ pi01/code/pi01.py
@@ -0,0 +1,130 @@
+
+import os, sys
+
+
+from skipole import WSGIApplication, FailPage, GoTo, ValidateError, ServerError, set_debug
+
+
+
+# the framework needs to know the location of the projectfiles directory holding this and
+# other projects - specifically the skis and skiadmin projects
+# The following line assumes, as default, that this file is located beneath
+# ...projectfiles/newproj/code/
+
+PROJECTFILES = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+PROJECT = 'pi01'
+
+
+from picode import control, login, database_ops, hardware
+
+# any page not listed here requires basic authentication
+_PUBLIC_PAGES = [1,  # index
+                 2,  # sensors
+                 4,  # information
+                 6,  # controls.json
+                 7,  # sensors.json
+                 9,  # sernsors_refresh,
+               540,  # no_javascript
+              1002,  # css
+              1004,  # css
+              1006   # css
+               ]
+
+
+# checks database exists, if not create it
+database_ops.start_database(PROJECT, PROJECTFILES)
+# setup hardware
+hardware.initial_setup_outputs()
+# get dictionary of initial start-up output values from database
+output_dict = database_ops.power_up_values()
+if not output_dict:
+    print("Invalid read of database, delete setup directory to revert to defaults")
+    sys.exit(1)
+# set the initial start-up values
+control.set_multi_outputs(output_dict)
+
+
+def start_call(called_ident, skicall):
+    "When a call is initially received this function is called."
+    if not called_ident:
+        return
+    if skicall.environ.get('HTTP_HOST'):
+        # This is used in the information page to insert the host into a displayed url
+        skicall.call_data['HTTP_HOST'] = skicall.environ['HTTP_HOST']
+    else:
+        skicall.call_data['HTTP_HOST'] = skicall.environ['SERVER_NAME']
+    # password protected pages
+    if called_ident[1] not in _PUBLIC_PAGES:
+        # check login
+        if not login.check_login(skicall.environ):
+            # login failed, ask for a login
+            return (skicall.project,2010)
+    return called_ident
+
+
+@use_submit_list
+def submit_data(skicall):
+    "This function is called when a Responder wishes to submit data for processing in some manner"
+    raise FailPage("submit_list string not recognised")
+
+
+def end_call(page_ident, page_type, skicall):
+    """This function is called at the end of a call prior to filling the returned page with page_data,
+       it can also return an optional ident_data string to embed into forms."""
+    # in this example, status is the value on input02
+    status = hardware.get_text_input('input02')
+    if status:
+        skicall.page_data['topnav','status', 'para_text'] = status
+    else:
+        skicall.page_data['topnav','status', 'para_text'] = "Status: input02 unavailable"
+    return
+
+
+# create the wsgi application
+application = WSGIApplication(project=PROJECT,
+                              projectfiles=PROJECTFILES,
+                              proj_data={},
+                              start_call=start_call,
+                              submit_data=submit_data,
+                              end_call=end_call,
+                              url="/")
+
+
+
+skis_code = os.path.join(PROJECTFILES, 'skis', 'code')
+if skis_code not in sys.path:
+    sys.path.append(skis_code)
+import skis
+skis_application = skis.makeapp(PROJECTFILES)
+application.add_project(skis_application, url='/lib')
+
+
+
+if __name__ == "__main__":
+
+    # If called as a script, this portion runs the python wsgiref.simple_server
+    # and serves the project. Typically you would do this with the 'skiadmin'
+    # sub project added which can be used to develop pages for your project
+
+    ############################### THESE LINES ADD SKIADMIN ######################
+                                                                                  #
+    set_debug(True)                                                               #
+    skiadmin_code = os.path.join(PROJECTFILES, 'skiadmin', 'code')                #
+    if skiadmin_code not in sys.path:                                             #
+        sys.path.append(skiadmin_code)                                            #
+    import skiadmin                                                               #
+    skiadmin_application = skiadmin.makeapp(PROJECTFILES, editedprojname=PROJECT) #
+    application.add_project(skiadmin_application, url='/skiadmin')                #
+                                                                                  #
+    ###############################################################################
+
+    from wsgiref.simple_server import make_server
+
+    # serve the application
+    host = "127.0.0.1"
+    port = 8000
+
+    httpd = make_server(host, port, application)
+    print("Serving %s on port %s. Call http://localhost:%s/skiadmin to edit." % (PROJECT, port, port))
+    httpd.serve_forever()
+
